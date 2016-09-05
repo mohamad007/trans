2a5c3a7df53eb52bc58508358ee6a262e4f274ea
mohamad007 <mohamad137977@gmail.com>
2016-09-05 03:00:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: '246195966:AAG0MAl4Bi5pMamAW0c6vdV2ZREEMovVehw',
+  apiKey: '',
   pollingTimeout: 20,
   startMessage: `سلام خوش آمدید من ربات مترجم هستم برای دریافت دستورات دستور/help را بفرستید `,
   masterId: 'YOUR_TELEGRAM_USER_ID (Something like 208094932)'
